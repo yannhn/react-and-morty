40e643182b0ef52350052fdeaaf379c8459a152c
Yannik Hangst <yanso1@posteo.de>
2022-05-26 22:28:27 +0200

    fix errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import "./App.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
-import Card from "./components/card";
+import { Card } from "./components/card";
+
 import { useState, useEffect } from "react";
 
 function App() {
-  const [persons, setPersons] = useState([]);
+  const [characters, setcharacters] = useState([]);
   useEffect(() => {
     const url = "https://rickandmortyapi.com/api/character/";
     fetchData(url);
     async function fetchData(url) {
       const response = await fetch(url);
       const data = await response.json();
-      setPersons(data.results);
+      setcharacters(data.results);
     }
   }, []);
 
   return (
     <>
       <Header></Header>;
-      {persons.map(({ id, image, name, status, species, type, gender }) => {
+      {characters.map(({ id, image, name, status, species, type, gender }) => {
         return (
           <Card
             key={id}
@@ -32,6 +33,7 @@ function App() {
           />
         );
       })}
+      <toggleButton></toggleButton>
       <Footer></Footer>;
     </>
   );
--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,26 @@
 import "./card.css";
+import { useState } from "react";
 
 function Card({ name, image, status, species, type, gender }) {
+  const [showDetails, setDetailsShown] = useState(false);
   return (
     <section className="card__background">
       <img src={image} className="card__image"></img>
       <h2 className="card__name">{name}</h2>
-      <button className="card__details">Show Details</button>
+      <button
+        className="card__details"
+        type="button"
+        onClick={() => {
+          setDetailsShown(!showDetails);
+        }}
+      >
+        {showDetails ? "Hide Details" : "Show Details"}
+      </button>
       <section className="card__attribute__background">
-        <p className="card__attribute">{status}</p>
-        <p className="card__attribute">{species}</p>
-        <p className="card__attribute">{gender}</p>
-        <p className="card__attribute">{type}</p>
+        <p className="card__attribute">{showDetails ? status : ""}</p>
+        <p className="card__attribute">{showDetails ? species : ""}</p>
+        <p className="card__attribute">{showDetails ? gender : ""}</p>
+        <p className="card__attribute">{showDetails ? type : ""}</p>
       </section>
       <svg class="card__bookmark" viewBox="0 0 24 24">
         <path d="M17,3H7A2,2 0 0,0 5,5V21L12,18L19,21V5C19,3.89 18.1,3 17,3Z" />
@@ -19,4 +29,4 @@ function Card({ name, image, status, species, type, gender }) {
   );
 }
 
-export default Card;
+export { Card };
